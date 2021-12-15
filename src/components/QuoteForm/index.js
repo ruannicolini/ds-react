35f7234c94cc70f770c4ee9425293edea6ccd406
ruannicolini-im <ruan@imaginationmedia.com>
2021-12-15 17:35:48 -0300

    Add Quote form style section

--- src/components/QuoteForm/index.js
+++ src/components/QuoteForm/index.js
@@ -1,3 +1,5 @@
+import salgadoImage from '../../assets/salgado.png';
+
 import './styles.scss';
 
 export function QuoteForm({textContent, ...props}) {
@@ -9,6 +11,26 @@ export function QuoteForm({textContent, ...props}) {
     // console.log('inputs',inputs );
 
     return (
-        <div className="quote-form"></div>
+        <div className="quote-form">
+
+            <div className="quote-form__content">
+                <div className="quote-form__image-wrapper">
+                    <img className='quote-form__image' src={salgadoImage} alt="Salgados" />
+                </div>
+                <div className="quote-form__form-wrapper">
+                    <div className="quote-form__form">
+                        <h2 class="quote-form__heading">{heading}</h2>
+
+                        {inputs && inputs.map( (inp) => (
+                            inp
+                        ))};
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="quote-form__footer-line"/>
+            <div className="quote-form__second-footer-line"/>
+        </div>
     );
 }
