232a254c7a5b5275165ed0d3973f609773431919
ruannicolini-im <ruan@imaginationmedia.com>
2022-01-03 11:39:14 -0300

    Update sticky bar style

--- src/hooks/useQuote.js
+++ src/hooks/useQuote.js
@@ -4,16 +4,16 @@ import { createContext, ReactNode, useContext, useEffect, useState } from "react
 const QuoteContext = createContext();
 
 const products = [
-    { name: "Coxinha 18g", additionalInfo: "Recheio: frango", price: .40},
-    { name: "Pastel 18g", additionalInfo: "Recheio: carne moída", price: .40},
-    { name: "Bolinha de queijo 18g", additionalInfo: "Recheio: queijo com milho", price: .40},
-    { name: "Bolinha de alho frito 18g", additionalInfo: "Recheio: queijo com alho", price: .40},
-    { name: "Bolinha de alho-poró 18g", additionalInfo: "Recheio: queijo com alho poró", price: .40},
-    { name: "Bolinha de bacalhau 18g", additionalInfo: "Contém bacalhau e batata", price: .40},
-    { name: "Quibe 18g", additionalInfo: "Contém carne e especiarias", price: .40},
-    { name: "Croquete 18g", additionalInfo: "Recheio: queijo e presunto", price: .40},
-    { name: "Croquete de calabresa 18g", additionalInfo: "Contém calabresa", price: .40},
-    { name: "Enroladinho de Salsicha 18g", additionalInfo: "Recheio: salsicha", price: .40}
+    { name: "Coxinha 18g", additionalInfo: "Recheio: frango", price: .42},
+    { name: "Pastel 18g", additionalInfo: "Recheio: carne moída", price: .42},
+    { name: "Bolinha de queijo 18g", additionalInfo: "Recheio: queijo com milho", price: .42},
+    { name: "Bolinha de alho frito 18g", additionalInfo: "Recheio: queijo com alho", price: .42},
+    { name: "Bolinha de alho-poró 18g", additionalInfo: "Recheio: queijo com alho poró", price: .42},
+    { name: "Bolinha de bacalhau 18g", additionalInfo: "Contém bacalhau e batata", price: .42},
+    { name: "Quibe 18g", additionalInfo: "Contém carne e especiarias", price: .42},
+    { name: "Croquete 18g", additionalInfo: "Recheio: queijo e presunto", price: .42},
+    { name: "Croquete de calabresa 18g", additionalInfo: "Contém calabresa", price: .42},
+    { name: "Enroladinho de Salsicha 18g", additionalInfo: "Recheio: salsicha", price: .42}
 ]
 
 export function QuoteProvider({children}) {
